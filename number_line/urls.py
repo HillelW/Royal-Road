40019b63fa7573e1e64e1b26addd23cfeb8b120f
hillelw <hillelwolin@gmail.com>
2020-03-09 23:14:37 -0400

    add nav bar and variation on the ticks

--- number_line/urls.py
+++ number_line/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.number_line, name='number_line'),
     path('blank_line', views.blank_line, name='blank_line'),
     path('line_with_ticks', views.line_with_ticks, name='line_with_ticks'),
+    path('line_with_two_ticks', views.line_with_two_ticks, name='line_with_two_ticks'),
     path('base', views.base, name='base'),
 ]
--- number_line/views.py
+++ number_line/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# plan is to create a single page application using below ideas
+
 def blank_line(request):
     return render(request, 'blank_line.html')
 
 def line_with_ticks(request):
     return render(request, 'line_with_ticks.html')
 
+def line_with_two_ticks(request):
+    return render(request, 'line_with_two_ticks.html')
+
 def base(request):
     return render(request, 'base.html')
 
 def number_line(request):
     return HttpResponse('This page will contain a number line...')
+
+
