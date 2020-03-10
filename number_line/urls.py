a1e0bbcd9f07a2bee7e1826a68156f86f034a957
hillelw <hillelwolin@gmail.com>
2020-03-09 21:57:01 -0400

    add blank line and line with ticks

--- number_line/urls.py
+++ number_line/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.number_line, name='number_line'),
+    path('blank_line', views.blank_line, name='blank_line'),
+    path('line_with_ticks', views.line_with_ticks, name='line_with_ticks'),
+    path('base', views.base, name='base'),
 ]
--- number_line/views.py
+++ number_line/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def blank_line(request):
+    return render(request, 'blank_line.html')
+
+def line_with_ticks(request):
+    return render(request, 'line_with_ticks.html')
+
+def base(request):
+    return render(request, 'base.html')
+
 def number_line(request):
     return HttpResponse('This page will contain a number line...')
-
-
-
